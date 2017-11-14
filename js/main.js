1553abc2808e4fc41ae64b90b9ecae1a47a14735
Coolesha <marekkulesza1@gmail.com>
2017-11-14 13:46:36 +0100

    slick slider script for avatars in testmonials section, slick files and img

---
+++ js/main.js
@@ -0,0 +1,10 @@
+    $(document).on('ready', function() {
+
+      $(".autoplay").slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+
+    });
